54439c246ebff5ca642d69c3db9a70a82bdfbd49
Kar9ti8k <karthiksankineni07@gmail.com>
2025-03-07 00:36:22 +0530

    completed Product Pricing

--- src/component/PageNav/PavgeNav.jsx
+++ src/component/PageNav/PavgeNav.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../PageNav/PavgeNav.css'
 import { NavLink } from 'react-router-dom'
-import Logo from '../../pages/Logo/Logo'
+import Logo from '../Logo/Logo'
 const PavgeNav = () => {
   return (
     <>
@@ -15,7 +15,9 @@ const PavgeNav = () => {
             <NavLink to='/product'>product</NavLink>
           </li>
           <li>
-            <NavLink to='/Login'>Login</NavLink>
+            <NavLink to='/Login' className='ctaLink'>
+              Login
+            </NavLink>
           </li>
         </ul>
       </nav>
